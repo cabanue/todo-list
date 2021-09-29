ec0e61c2f78783dedc09b0bf1b6e08eb0757bd89
cabanue <cabanue@iu.edu>
2021-09-29 12:31:09 -0400

    added prefix to paths

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 Carlos Banuelos
 
-Live Link: [Todo List App](https://in-info-web4.informatics.iupui.edu/~cabanue/n322)
-
-Readme for different branch
+Live Link: [Todo List App](https://in-info-web4.informatics.iupui.edu/~cabanue/n322/todo-list/)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl:
+      "https://in-info-web4.informatics.iupui.edu/~cabanue/n322/todo-list/",
     title: "TODO List",
   },
   plugins: [],
+  pathPrefix: "/~cabanue/n322/todo-list",
 };
